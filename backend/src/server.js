47a22ed48791ce69d13bfd3db396640a3f914b37
SpookyBee <hoangphu2234@gmail.com>
2026-06-27 21:26:13 +0700

    sửa lần 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV !== "production") {
 app.use("/api/task", taskRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "./../frontend/dist")));
 
   app.get("/{*splat}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
