3ef9236675353abcfd249e1044ada8342ae94678
SpookyBee <hoangphu2234@gmail.com>
2026-06-27 20:58:34 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,20 +3,32 @@ import taskRoute from "../Router/tasksRoute.js";
 import dotenv from "dotenv";
 import { connectDB } from "../config/connectDB.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
 // app.use(cors()); Cho phép tất cả domains gọi APIs
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use("/api/task", taskRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
 connectDB().then(() => {
-  app.listen("3000", () => {
+  app.listen(process.env.PORT || "3000", () => {
     console.log("Server started ...");
   });
 });
